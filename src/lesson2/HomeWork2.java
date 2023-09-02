a3d0d9cf3669c32368434ba4d3d659c2e3c486f7
OlgaVoskivarova <ovvoskivarova@gmail.com>
2023-09-02 23:00:54 +0200

    HomeWorkJava2

---
+++ src/lesson2/HomeWork2.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        int x = 345;
+
+        int num1 = x/100;
+        int num2 =x % 100/10;
+        int num3 = x%10;
+
+        System.out.println("Число:" + x +">" + num1 + ","+ num2 + "," + num3);
+
+    }
+}
